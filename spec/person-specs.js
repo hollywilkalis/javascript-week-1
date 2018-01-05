1c1efdc5c18a8c689a2a693ab72c6b5a5c827fe4
Holly Wilkalis <holly.wilkalis@gmail.com>
2018-01-05 14:36:00 -0800

    Add 2 testing specs

---
+++ spec/person-specs.js
@@ -0,0 +1,17 @@
+var Person = require('./../js/scripts.js').personModule;
+
+describe('Person', function() {
+  it('should test whether the input age is being received and stored', function() {
+    var person = new Person("Holly",10)
+    expect(person.age).toEqual(10)
+    expect(person.age).not.toEqual(11)
+  });
+});
+
+describe('Person', function() {
+  it('should disregard non-alpha', function() {
+    var person = new Person("Holly",10)
+    expect(person.calculateSeconds()).toEqual(315360000);
+    expect(person.calculateSeconds()).not.toEqual(315360100);
+  });
+});
