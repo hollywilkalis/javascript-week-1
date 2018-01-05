eb26607fbb92f1eadf5e8e9b249a070ad09c571f
Holly Wilkalis <holly.wilkalis@gmail.com>
2018-01-05 14:52:02 -0800

    Set up karma and get two working test specs

--- spec/person-specs.js
+++ spec/person-specs.js
@@ -9,7 +9,7 @@ describe('Person', function() {
 });
 
 describe('Person', function() {
-  it('should disregard non-alpha', function() {
+  it('should test whether the input age is being correctly converted from years to seconds', function() {
     var person = new Person("Holly",10)
     expect(person.calculateSeconds()).toEqual(315360000);
     expect(person.calculateSeconds()).not.toEqual(315360100);
